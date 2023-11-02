c40dc96246da3014ac30ba05c4f0ed872c5bc441
MUHAMMAD NUR SOLIHIN BIN MALIK RADZUAN <muhammadnursolihin@graduate.utm.my>
2023-11-02 21:36:15 +0800

    Change dashboard, login and register
    Change dashboard, login and register

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,60 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <!-- Basic Page Info -->
+    <meta charset="utf-8">
+    <title>Crunchy Code</title>
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+
+    <!-- Scripts -->
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+
+    <!-- Additional -->
+   
+    <!-- Site favicon -->
+    <link rel="apple-touch-icon" sizes="180x180" href="../../../vendors/images/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="../../../vendors/images/favicon-32x32.png">
+    <link rel="icon" type of="image/png" sizes="16x16" href="../../../vendors/images/favicon-16x16.png">
+
+    <!-- Mobile Specific Metas -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+    <!-- Google Font -->
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
+    <!-- CSS -->
+    <link rel="stylesheet" type="text/css" href="../../../vendors/styles/core.css">
+    <link rel="stylesheet" type="text/css" href="../../../vendors/styles/icon-font.min.css">
+    <link rel="stylesheet" type="text/css" href="../../../vendors/styles/style.css">
+ 
+</head>
+<body>
+    
+    @include('components.preloader')
+    
+    @include('components.header')
+
+    @include('components.right-sidebar')
+
+    @include('components.left-sidebar')
+                
+    <main>
+        @yield('content')
+    </main>
+  
+
+    <!-- js -->
+	<script src="../../../vendors/scripts/core.js"></script>
+	<script src="../../../vendors/scripts/script.min.js"></script>
+	<script src="../../../vendors/scripts/process.js"></script>
+	<script src="../../../vendors/scripts/layout-settings.js"></script>
+    
+</body>
+</html>
