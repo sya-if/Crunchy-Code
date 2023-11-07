c51fcc8263e9fe22c826f9156a3a2154d9435312
MUHAMMAD NUR SOLIHIN BIN MALIK RADZUAN <muhammadnursolihin@graduate.utm.my>
2023-11-07 20:40:18 +0800

    ghfg
    fghfg

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -32,105 +32,58 @@
                 </div>
             </div>
         </div>
-
-
         <div class="card-box pd-20 height-100-p mb-30">
             <div class="row align-items-center">
-                <div class="col-md-3">
-                    <img src="vendors/images/photo1.jpg" alt="">
+                <div class="col-md-4">
+                    <img src="vendors/images/banner-img.png" alt="">
                 </div>
-                <div class="col-md-6">
+                <div class="col-md-8">
                     <h4 class="font-20 weight-500 mb-10 text-capitalize">
                         Welcome back <div class="weight-600 font-30 text-blue">{{ old('name' , $user->nickname ) }}</div>
                     </h4>
-                    <p class="font-18 max-width-600"> </p>
+                    <p class="font-18 max-width-600">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde hic non repellendus debitis iure, doloremque assumenda. Autem modi, corrupti, nobis ea iure fugiat, veniam non quaerat mollitia animi error corporis.</p>
                 </div>
             </div>
         </div>
-    
-        <div class="card-box pd-20 height-100-p mb-30">
-            <div class="row align-items-center">
-                <div class="progress-tracker">
-                    <h2>Progress</h2>
-                    <div class="row clearfix progress-box">
-                        <div class="col-lg-3 col-md-6 col-sm-12 mb-30">
-                            <div class="card-box pd-30 height-100-p">
-                                <div class="progress-box text-center">
-                                    <input type="text" class="knob dial1" value="80" data-width="120" data-height="120" data-linecap="round" data-thickness="0.12" data-bgColor="#fff" data-fgColor="#1b00ff" data-angleOffset="180" readonly>
-                                    <h5 class="text-blue padding-top-10 h5">Module 1</h5>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-lg-3 col-md-6 col-sm-12 mb-30">
-                            <div class="card-box pd-30 height-100-p">
-                                <div class="progress-box text-center">
-                                    <input type="text" class="knob dial2" value="70" data-width="120" data-height="120" data-linecap="round" data-thickness="0.12" data-bgColor="#fff" data-fgColor="#00e091" data-angleOffset="180" readonly>
-                                    <h5 class="text-light-green padding-top-10 h5">Module 2</h5>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-lg-3 col-md-6 col-sm-12 mb-30">
-                            <div class="card-box pd-30 height-100-p">
-                                <div class="progress-box text-center">
-                                    <input type="text" class="knob dial3" value="90" data-width="120" data-height="120" data-linecap="round" data-thickness="0.12" data-bgColor="#fff" data-fgColor="#f56767" data-angleOffset="180" readonly>
-                                    <h5 class="text-light-orange padding-top-10 h5">Module 3</h5>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-lg-3 col-md-6 col-sm-12 mb-30">
-                            <div class="card-box pd-30 height-100-p">
-                                <div class="progress-box text-center">
-                                    <input type="text" class="knob dial4" value="65" data-width="120" data-height="120" data-linecap="round" data-thickness="0.12" data-bgColor="#fff" data-fgColor="#a683eb" data-angleOffset="180" readonly>
-                                    <h5 class="text-light-purple padding-top-10 h5">Module 4</h5>
-                                </div>
-                            </div>
-                        </div>
+        <div class="row clearfix progress-box">
+            <div class="col-lg-3 col-md-6 col-sm-12 mb-30">
+                <div class="card-box pd-30 height-100-p">
+                    <div class="progress-box text-center">
+                         <input type="text" class="knob dial1" value="80" data-width="120" data-height="120" data-linecap="round" data-thickness="0.12" data-bgColor="#fff" data-fgColor="#1b00ff" data-angleOffset="180" readonly>
+                        <h5 class="text-blue padding-top-10 h5">My Earnings</h5>
+                        <span class="d-block">80% Average <i class="fa fa-line-chart text-blue"></i></span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-12 mb-30">
+                <div class="card-box pd-30 height-100-p">
+                    <div class="progress-box text-center">
+                         <input type="text" class="knob dial2" value="70" data-width="120" data-height="120" data-linecap="round" data-thickness="0.12" data-bgColor="#fff" data-fgColor="#00e091" data-angleOffset="180" readonly>
+                        <h5 class="text-light-green padding-top-10 h5">Business Captured</h5>
+                        <span class="d-block">75% Average <i class="fa text-light-green fa-line-chart"></i></span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-12 mb-30">
+                <div class="card-box pd-30 height-100-p">
+                    <div class="progress-box text-center">
+                         <input type="text" class="knob dial3" value="90" data-width="120" data-height="120" data-linecap="round" data-thickness="0.12" data-bgColor="#fff" data-fgColor="#f56767" data-angleOffset="180" readonly>
+                        <h5 class="text-light-orange padding-top-10 h5">Projects Speed</h5>
+                        <span class="d-block">90% Average <i class="fa text-light-orange fa-line-chart"></i></span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-12 mb-30">
+                <div class="card-box pd-30 height-100-p">
+                    <div class="progress-box text-center">
+                         <input type="text" class="knob dial4" value="65" data-width="120" data-height="120" data-linecap="round" data-thickness="0.12" data-bgColor="#fff" data-fgColor="#a683eb" data-angleOffset="180" readonly>
+                        <h5 class="text-light-purple padding-top-10 h5">Panding Orders</h5>
+                        <span class="d-block">65% Average <i class="fa text-light-purple fa-line-chart"></i></span>
                     </div>
                 </div>
             </div>
         </div>
 
-        <div class="card-box pd-20 height-100-p mb-30">
-            <div class="row align-items-center">
-
-                <div class="recommended-courses">
-                    <h2>Recommended Courses</h2>
-                    <div class="course">
-                        <div class="card" style="width: 18rem;">
-                            <div class="card-body">
-                                <h5 class="card-title">Module 1</h5>
-                                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                                <a href="#" class="btn btn-primary">Go somewhere</a>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="course">
-                        <div class="card" style="width: 18rem;">
-                            <div class="card-body">
-                                <h5 class="card-title">Module 2</h5>
-                                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                                <a href="#" class="btn btn-primary">Subsribe This Course</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <div class="card-box pd-20 height-100-p mb-30">
-            <div class="row align-items-center">
-                <div class="announcements">
-                    <h2>Latest Announcements</h2>
-                    <div class="announcement">
-                        <p>[Announcement 1]</p>
-                    </div>
-                    <div class="announcement">
-                        <p>[Announcement 2]</p>
-                    </div>
-                </div>
-            </div>
-        </div>
     </div>
 </div>
 
