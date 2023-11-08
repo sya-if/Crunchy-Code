55715edf1938348859f7aa00f459059b40d97eaf
MUHAMMAD NUR SOLIHIN BIN MALIK RADZUAN <muhammadnursolihin@graduate.utm.my>
2023-11-08 11:50:15 +0800

    scs
    scs

--- resources/views/admin/create.blade.php
+++ resources/views/admin/create.blade.php
@@ -30,38 +30,85 @@
                 </div>
 
                 <div class="row">
-                    <div class="col-md-6">
-                        <!-- Student creation form -->
-                        <form method="post" action="{{ route('user.store') }}">
-                            
-                            @csrf <!-- CSRF token -->
 
-                            <!-- Student Name -->
-                            <div class="form-group">
-                                <label for="fullname">Student Name</label>
-                                <input type="text" id="fullname" name="fullname" class="form-control" placeholder="Enter Student Name">
+                    <div class="card-body">
+                        <form method="POST" action="{{ route('user.store') }}">
+                            @csrf
+
+                            <!-- Full Name -->
+                            <div class="form-group row">
+                                <label for="fullname" class="col-md-4 col-form-label">{{ __('Fullname') }}</label>
+                                <div class="col-md-8">
+                                    <input id="fullname" type="text" class="form-control @error('fullname') is-invalid @enderror" name="fullname" value="{{ old('fullname') }}" required autocomplete="fullname" autofocus>
+
+                                    @error('fullname')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
                             </div>
 
-                            <!-- Email -->
-                            <div class="form-group">
-                                <label for="email">Email</label>
-                                <input type="email" id="email" name="email" class="form-control" placeholder="Enter Email">
+                            <!-- Nick Name -->
+                            <div class="form-group row">
+                                <label for="nickname" class="col-md-4 col-form-label">{{ __('Nickname') }}</label>
+                                <div class="col-md-8">
+                                    <input id="nickname" type="text" class="form-control @error('nickname') is-invalid @enderror" name="nickname" value="{{ old('nickname') }}" required autocomplete="nickname" autofocus>
+
+                                    @error('nickname')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
                             </div>
 
-                            <!-- Phone Number -->
-                            <div class="form-group">
-                                <label for="phone">Phone Number</label>
-                                <input type="text" id="phone" name="phone" class="form-control" placeholder="Enter Phone Number">
+                            <!-- Email Address -->
+                            <div class="form-group row">
+                                <label for="email" class="col-md-4 col-form-label">{{ __('Email Address') }}</label>
+                                <div class="col-md-8">
+                                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+
+                                    @error('email')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
                             </div>
 
                             <!-- Password -->
-                            <div class="form-group">
-                                <label for="password">Password</label>
-                                <input type="password" id="password" name="password" class="form-control" placeholder="Enter Password">
+                            <div class="form-group row">
+                                <label for="password" class="col-md-4 col-form-label">{{ __('Password') }}</label>
+                                <div class="col-md-8">
+                                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+
+                                    @error('password')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <!-- Confirm Password -->
+                            <div class="form-group row">
+                                <label for="password-confirm" class="col-md-4 col-form-label">{{ __('Confirm Password') }}</label>
+                                <div class="col-md-8">
+                                    <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                                </div>
                             </div>
 
                             <!-- Submit Button -->
-                            <button type="submit" class="btn btn-primary">Create Student</button>
+                            <div class="form-group row mb-0">
+                                <div class="col-md-8 offset-md-4">
+                                    <button type="submit" class="btn btn-primary">
+                                        {{ __('Register') }}
+                                    </button>
+                                </div>
+                            </div>
+
+
                         </form>
                     </div>
                 </div>
