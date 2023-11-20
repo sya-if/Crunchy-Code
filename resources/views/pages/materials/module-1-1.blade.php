032ea89b67b6b5099f7a8fbd5dc66b0ac5fc0b96
MUHAMMAD NUR SOLIHIN BIN MALIK RADZUAN <muhammadnursolihin@graduate.utm.my>
2023-11-20 20:24:23 +0800

    dsdsd
    sdsd

--- resources/views/pages/materials/module-1-1.blade.php
+++ resources/views/pages/materials/module-1-1.blade.php
@@ -111,6 +111,19 @@
     margin: 0;
 }
 
+.image-size
+{
+    width: 55%;
+    margin-left: auto;
+    margin-right: auto;
+    display: block;
+}
+
+.text-formatting
+{
+    text-align: justify;
+}
+
 </style>
 
 
@@ -257,82 +270,108 @@
                                     
                                     <div class="blog-caption" >
     
-                                        <!-- Heading -->
+                                        <!-- Pengenalan -->
                                         <h4 class="mb-10"> Module 1.1.2 Keperluan Penyelesaian Masalah Berstrategi </h4>
-                                        <p>
-                                            Pengenalan membawa kita ke dalam dunia yang menggembirakan dan penting dalam pengaturcaraan - strategi penyelesaian masalah. 
-                                            Dalam bab ini, kita akan memahami betapa pentingnya kemampuan untuk merumuskan dan menyelesaikan masalah dalam konteks pengaturcaraan. 
-                                            Strategi penyelesaian masalah membentuk landasan utama bagi setiap pengaturcara yang berjaya.
+                                        <p class="text-formatting">
+                                            Pelbagai masalah dalam kehidupan harian merupakan cabaran yang perlu diatasi. Masalah menghalang kita daripada mencapai apa yang diingini, di samping mengganggu kelancaran dan keselesaan hidup. Sesuatu masalah yang rumit memerlukan strategi penyelesaian masalah yang berkesan. Apakah faedah penggunaan strategi dalam penyelesaian masalah? 
+                                        </p>
+
+                                        <div class="card image-size" align="center">
+                                            <img class="card-img-top" src="../../../vendors/images/Module/keperluan-1-2.jpg" alt="Card image cap">
+                                        </div>
+
+                                        <p class="text-center" style="color:#ff0000;"> Rajah: Keperluan Penyelesaian Masalah </p>
+                                        
+                                        <p class="text-formatting">
+                                            Pembangunan aplikasi komputer berkait rapatdengan strategi penyelesaian masalah dalam pengaturcaraan komputer. Seseorang perlu menguasai strategi penyelesaian masalah supaya dapat memahami masalah dengan mudah bagi mendapatkan strategi penyelesaian yang terbaik. 
+                                        </p>
+
+                                        <p class="text-formatting" style="border: 2px solid #B4BDFF; padding: 10px; border-radius:8px;">
+                                            <b>Penyelesaian masalah</b> sebagai proses mengkaji butiran sesuatu masalah untuk mendapatkan satu penyelesaian (Businessdictionary.com)
                                         </p>
                                         
-                                        <!-- Definisi Masalah -->
-                                        <h5 class="mb-10">Definisi Masalah </h5>
-                                        <p>
-                                            Masalah adalah keraguan, situasi yang tidak diingini, cabaran dan peluang yang dihadapi dalam kehidupan seseorang. 
-                                            Kemahiran membuat keputusan amat diperlukan untuk melahirkan generasi yang berkebolehan merancang strategi penyelesaian masalah. 
-                                            Contohnya, kita perlu membuat keputusan tentang jenis makanan yang hendak dibeli di kantin dengan jumlah wang yang ada. 
+                                        <p class="text-formatting">
+                                            Komputer boleh digunakan untuk menyelesaikan masalah. Waiau bagaimanapun, cara penyelesaian terhadap masalah yang dihadapi haruslah jelas sebelum mengatasinya. Pemikiran komputasional (computational) membolehkan kita memahami masalah yang dihadapi dan membangunkan penyelesaian yang munasabah apabila kita menghadapi masalah yang kompleks. 
                                         </p>
-    
-                                        <!-- Definisi Penyelesaian Masalah -->
-                                        <h5 class="mb-10">Definisi Penyelesaian Masalah </h5>
-                                        <p>
-                                            Penyelesaian masalah ialah tunjang utama dalam sains komputer. Pengatur cara perlu memahami cara penyelesaian sesuatu masalah 
-                                            dan menterjemahkan cara tersebut kepada algoritma yang dapat difahami oleh komputer. Penyelesaian masalah sebagai proses mengkaji butiran sesuatu masalah untuk mendapatkan satu penyelesaian (Businessdictionary.com) 
+                                        
+                                        <!-- Teknik Pemikiran Komputasional -->
+                                        <h4 class="mb-10"> Teknik Pemikiran Komputasional </h4>
+
+                                        <!-- Teknik Leraian -->
+                                        <h5>1. Teknik Leraian </h5><br>
+
+                                        <p class="text-formatting">
+                                            Memecahkan masalah kepada bahagian yang lebih kecil dan terkawal. Sebelum komputer menyelesaikan masalah, masalah dan cara untuk menyelesaikannya mestilah difahami. 
+                                            Penguraian masalah membantu memecahkan masalah kompleks kepada bahagian yang mudah diuruskan. Begitu juga, kita cuba memahami bagaimana sistem yang kompleks berfungsi menggunakan teknik leraian.
+                                            Contoh: Memahami cara sebuah basikal berfungsi akan jadi lebih mudah jika seluruh basikal itu dipecahkan kepada bahagian yang kecil. Rujuk rajah di bawah dan labelkan bahagian basikal yang dilukiskan.
                                         </p>
-    
-    
-                                        <!-- Gambaran Penyelesaian Masalah -->
-                                        <div class="card-deck mb-30">
-                                             <!-- Image 1 Container -->
-                                            <div class="card">
-                                                <img class="card-img-top" src="../../../vendors/images/Module/flowchart1.png" alt="Card image cap">
-                                            </div>
-    
-                                            <!-- Image 2 Container -->
-                                            <div class="card">
-                                                <img class="card-img-top" src="../../../vendors/images/Module/code1.png" alt="Card image cap">
-                                                <div class="card-body">
-    
-                                                    <div class="col-lg-6 col-sm-12 mb-30">
-                                                        <div class="pd-20 card-box height-100-p text-center">
-    
-                                                            <a href="#" class="button-85 btn-block" data-toggle="modal" data-target="#small-modal" type="button">
-                                                                Run
-                                                            </a>
-    
-                                                            <div class="modal fade" id="small-modal" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
-                                                                <div class="modal-dialog modal-sm modal-dialog-centered">
-                                                                    <div class="modal-content">
-                                                                        <div class="modal-header">
-                                                                            <h4 class="modal-title" id="myLargeModalLabel">Output</h4>
-                                                                            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-                                                                        </div>
-                                                                        <div class="modal-body">
-                                                                            <p> Hello Malaysia</p>
-                                                                        </div>
-                                                                        <div class="modal-footer">
-                                                                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                                                                        </div>
-                                                                    </div>
-                                                                </div>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-    
-                                                    <p align="justify">
-                                                        Anda boleh menekan butang di atas untuk melihat output yang akan dihasilkan sekiranya kita menjalankan kod tersebut di sebarang IDE.    
-                                                    </p>
-                                                </div>
-                                            </div>
+
+                                        <div class="card image-size" align="center">
+                                            <img class="card-img-top" src="../../../vendors/images/Module/teknik-pemecahan.jpg" alt="Card image cap">
                                         </div>
-    
-    
-    
-    
+
+                                        <p class="text-center" style="color:#ff0000;"> Rajah: Teknik leraian memecahkan basikal kepada bahagian-bahagian basikal </p>
+                                    
+                                        <!-- Teknik Leraian -->
+                                        <h5>2. Pengecaman Corak (Pattern Recognition) </h5><br>
+
+                                        <p class="text-formatting">
+                                            Mencari persamaan similaritieSJ antara masalah dan dalam masalah. Apabila kita telah meleraikan masalah yang kompleks kepada bahagian yang mudah urus dengan teknik leraian, ini membantu dalam mengenal pasti persamaan atau corak yang ada pada setiap bahagian masalah yang kecil. Corak-corak ini dapat membantu menyelesaikan masalah kompleks dengan lebih efisien.
+                                            Bayangkan kita hendak mengetahui cara sesebuah basikal berfungsi. Ciri-ciri basikal ialah mempunyai dua buah roda yang berputar, 
+                                            hendal untuk menguasai hala tuju, pedal untuk menggerakkan basikal dan rantai yang menjadi penyambung supaya basikal dapat berfungsi. 
+                                            Jadi, kita boleh melukis gambar basikal berdasarkan ciri-ciri di atas. Oalam pemikiran komputasional, ciri-ciri ini dikenal sebagai corak. Cuma yang berbeza secara spesifik ialah warna basikal, bunyi loceng atau saiz roda.
+                                        </p>
+
+                                        <div class="card image-size" align="center">
+                                            <img class="card-img-top" src="../../../vendors/images/Module/pengecaman-corak.png" alt="Card image cap">
+                                        </div>
+
+                                        <p class="text-center" style="color:#ff0000;"> Rajah: Teknik pengecaman corak di basikal </p>
+
+                                        <!-- Teknik Leraian -->
+                                        <h5>3. Peniskalaan (Abstraction) </h5><br>
+
+                                        <p class="text-formatting">
+                                            Peniskalaan adalah penting supaya dapat menjana penyelesaian yang tepat kepada masalah yang dihadapi. 
+                                            Contohnya, untuk menghasilkan sebuah basikal, jika tidak meniskalakan ciri-ciri umum dan spesifik, kita berkemungkinan akan menghasilkan basikal yang beroda kecil dan berbakul di hendalnya,
+                                            iaitu merupakan satu penyelesaian yang tidak tepat. Satu model basikal ialah basikal yang mempunyai dua roda, hendal dan pedal. Ciri spesifik seperti saiz roda, berbakul atau berloceng dan warna rangka hendaklah diabaikan.
+                                        </p>
+
+                                        <div class="card image-size" align="center">
+                                            <img class="card-img-top" src="../../../vendors/images/Module/teknik-peniskalaan.png" alt="Card image cap">
+                                        </div>
+
+                                        <p class="text-center" style="color:#ff0000;"> Rajah: Teknik peniskalaan di basikal </p>
+                                        
+
+                                        <!-- Teknik Algoritma -->
+                                        <h5>4. Algoritma (Algorithms) </h5><br>
+
+                                        <p class="text-formatting">
+                                            Membangunkan penyelesaian langkah demi langkah terhadap masalah yang dihadapi atau mematuhi peraturan untuk menyelesaikan masalah. Algoritma merupakan suatu pelan, iaitu satu set langkah arahan untuk menyelesaikan masalah. Jika anda boleh mengikat tali kasut, membuat secawan kopi, atau menyediakan makanan, anda telah tahu mengikut sesuatu algoritma.
+                                            Dalam algoritma, setiap arahan dikenal pasti dan susunan pelaksanaannya dijalankan seperti yang dirancang. Algoritma juga digunakan sebagai titik mula untuk rekaan program komputer dalam bentuk carta alir dan pseudokod.
+                                        </p>
+
+                                        <!-- Coding Displayed -->
+                                        <div class="terminal" style="width:70%;">
+                                            <p>
+
+                                                &nbsp;Mula<br>
+                                                &nbsp;&nbsp;OUPUT "Mempunyai 2 roda yang berputar?" <br>
+                                                &nbsp;&nbsp;INPUT "Ya" <br>
+                                                &nbsp;&nbsp;OUTPUT "Hendal untuk menguasai hala tuju?"<br>
+                                                &nbsp;&nbsp;INPUT "Ya" <br>
+                                                &nbsp;&nbsp;OUTPUT "Pedal dan rantai untuk menggerakkan basikal? <br>
+                                                &nbsp;&nbsp;INPUT "Ya" <br>
+                                                &nbsp;&nbsp;OUTPUT "Ini ialah sebuah basikal." <br>
+                                                &nbsp;Tamat<br>               
+                                            </p>
+                                        </div>
+
+                                        <p class="text-center" style="color:#ff0000;"> Rajah: Teknik algoritma untuk menentukan aspek basikal </p>
                                     </div>
                                 </div>
-                            </div>
-                           
+                            </div>                         
                         </div>
                     </div>
                 </div>
