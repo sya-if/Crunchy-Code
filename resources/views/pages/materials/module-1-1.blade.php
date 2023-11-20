c6ef771555c4aedf901e09a2405623a985c72d80
MUHAMMAD NUR SOLIHIN BIN MALIK RADZUAN <muhammadnursolihin@graduate.utm.my>
2023-11-20 19:34:03 +0800

    sasa
    sdas

--- resources/views/pages/materials/module-1-1.blade.php
+++ resources/views/pages/materials/module-1-1.blade.php
@@ -4,6 +4,8 @@
 
 @section('content')
 <style>
+
+
 .card {
     display: inline-block;
     margin: 10px; /* Adjust margin as needed */
@@ -82,10 +84,38 @@
     padding: 10px !important;
 }
 
+.shadow-box
+{
+    box-shadow: rgba(0, 0, 0, 0.07) 0px 1px 2px, rgba(0, 0, 0, 0.07) 0px 2px 4px, rgba(0, 0, 0, 0.07) 0px 4px 8px, rgba(0, 0, 0, 0.07) 0px 8px 16px, rgba(0, 0, 0, 0.07) 0px 16px 32px, rgba(0, 0, 0, 0.07) 0px 32px 64px;
+}
+
+.colour-box
+{
+   background: lightsteelblue;
+}
+
+.terminal 
+{
+    background-color: #2e2e2e;
+    color: #00ff00; /* Green text */
+    font-family: 'Courier New', monospace;
+    border-radius: 8px;
+    overflow: hidden;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+    margin: auto;
+}
+
+.terminal p 
+{
+    padding: 20px;
+    margin: 0;
+}
+
 </style>
 
 
 <div class="main-container">
+
     <div class="pd-ltr-20 xs-pd-20-10">
         <div class="min-height-200px">
 
@@ -97,8 +127,8 @@
                         </div>
                         <nav aria-label="breadcrumb" role="navigation">
                             <ol class="breadcrumb">
-                                <li class="breadcrumb-item"><a href="index.html">Home</a></li>
-                                <li class="breadcrumb-item" aria-current="page">Module</li>
+                                <li class="breadcrumb-item"><a href="{{route('home')}}">Home</a></li>
+                                <li class="breadcrumb-item" aria-current="{{route('module')}}">Module</li>
                                 <li class="breadcrumb-item active" aria-current="page">Module 1.1 Strategi Penyelesaian Masalah</li>
                             </ol>
                         </nav>
@@ -106,16 +136,17 @@
                 </div>
             </div>
 
-            <div class="blog-wrap">
+            <div>
                 <div class="container pd-0">
                     <div class="row">
                         <div class="col-md-12 col-sm-12">
+                            
+                            <div class="card-box blog-caption mb-20 shadow-box colour-box" style="padding:30px;" align="center">
+                                <h3>Module 1.1: Strategi Penyelesaian Masalah</h3>
+                            </div>
+
                             <div class="blog-detail card-box overflow-hidden mb-30">
 
-                                <div class="blog-img" style="margin-bottom:20px">
-                                    <img src="../../../vendors/images/Module/module1-1-landing-banner.png" alt="" style="width: 100%; height: auto;">
-                                </div>
-                                
                                 <div class="blog-caption" >
 
                                     <!-- Heading -->
@@ -151,7 +182,30 @@
 
                                         <!-- Image 2 Container -->
                                         <div class="card">
-                                            <img class="card-img-top" src="../../../vendors/images/Module/code1.png" alt="Card image cap">
+                                            
+                                            <!-- Coding Displayed -->
+                                            <div class="terminal">
+                                                <p>
+                                                    public class HelloMalaysia <br>
+                                                    { <br>
+                                                        &nbsp;public static void main(String[] args) <br>
+                                                        &nbsp;{ <br>
+                                                        &nbsp;&nbsp;//Declare the data type<br>
+                                                        &nbsp;&nbsp;<b>String x</b><br><br>
+                                                        &nbsp;&nbsp;//Berikan variable data untuk disimpan<br>
+                                                        &nbsp;&nbsp;<b>String x</b><br><br>
+                                                        &nbsp;&nbsp;//Paparkan data<br>
+                                                        &nbsp;&nbsp;<b>System.out.println(x);</b><br>
+                                                        &nbsp;} <br>
+                                                    } <br>
+                                                </p>
+                                            </div>
+
+                                            <br>
+                                            <p>
+                                                Anda boleh menekan butang di bawah untuk melihat output yang akan dihasilkan sekiranya kita menjalankan kod tersebut di sebarang IDE.    
+                                            </p>
+
                                             <div class="card-body">
 
                                                 <div class="col-lg-6 col-sm-12 mb-30">
@@ -179,25 +233,110 @@
                                                         </div>
                                                     </div>
                                                 </div>
-
-                                                <p align="justify">
-                                                    Anda boleh menekan butang di atas untuk melihat output yang akan dihasilkan sekiranya kita menjalankan kod tersebut di sebarang IDE.    
-                                                </p>
                                             </div>
                                         </div>
                                     </div>
-
-
-
-
                                 </div>
                             </div>
-                        </div>
-                       
+                        </div>                       
                     </div>
                 </div>
             </div>
         </div>
+
+
+        <!-- Module 1.1.2 -->
+        <div class="pd-ltr-20 xs-pd-20-10">
+            <div class="min-height-200px">
+
+                <div class="blog-wrap">
+                    <div class="container pd-0">
+                        <div class="row">
+                            <div class="col-md-12 col-sm-12">
+                                <div class="blog-detail card-box overflow-hidden mb-30">
+                                    
+                                    <div class="blog-caption" >
+    
+                                        <!-- Heading -->
+                                        <h4 class="mb-10"> Module 1.1.2 Keperluan Penyelesaian Masalah Berstrategi </h4>
+                                        <p>
+                                            Pengenalan membawa kita ke dalam dunia yang menggembirakan dan penting dalam pengaturcaraan - strategi penyelesaian masalah. 
+                                            Dalam bab ini, kita akan memahami betapa pentingnya kemampuan untuk merumuskan dan menyelesaikan masalah dalam konteks pengaturcaraan. 
+                                            Strategi penyelesaian masalah membentuk landasan utama bagi setiap pengaturcara yang berjaya.
+                                        </p>
+                                        
+                                        <!-- Definisi Masalah -->
+                                        <h5 class="mb-10">Definisi Masalah </h5>
+                                        <p>
+                                            Masalah adalah keraguan, situasi yang tidak diingini, cabaran dan peluang yang dihadapi dalam kehidupan seseorang. 
+                                            Kemahiran membuat keputusan amat diperlukan untuk melahirkan generasi yang berkebolehan merancang strategi penyelesaian masalah. 
+                                            Contohnya, kita perlu membuat keputusan tentang jenis makanan yang hendak dibeli di kantin dengan jumlah wang yang ada. 
+                                        </p>
+    
+                                        <!-- Definisi Penyelesaian Masalah -->
+                                        <h5 class="mb-10">Definisi Penyelesaian Masalah </h5>
+                                        <p>
+                                            Penyelesaian masalah ialah tunjang utama dalam sains komputer. Pengatur cara perlu memahami cara penyelesaian sesuatu masalah 
+                                            dan menterjemahkan cara tersebut kepada algoritma yang dapat difahami oleh komputer. Penyelesaian masalah sebagai proses mengkaji butiran sesuatu masalah untuk mendapatkan satu penyelesaian (Businessdictionary.com) 
+                                        </p>
+    
+    
+                                        <!-- Gambaran Penyelesaian Masalah -->
+                                        <div class="card-deck mb-30">
+                                             <!-- Image 1 Container -->
+                                            <div class="card">
+                                                <img class="card-img-top" src="../../../vendors/images/Module/flowchart1.png" alt="Card image cap">
+                                            </div>
+    
+                                            <!-- Image 2 Container -->
+                                            <div class="card">
+                                                <img class="card-img-top" src="../../../vendors/images/Module/code1.png" alt="Card image cap">
+                                                <div class="card-body">
+    
+                                                    <div class="col-lg-6 col-sm-12 mb-30">
+                                                        <div class="pd-20 card-box height-100-p text-center">
+    
+                                                            <a href="#" class="button-85 btn-block" data-toggle="modal" data-target="#small-modal" type="button">
+                                                                Run
+                                                            </a>
+    
+                                                            <div class="modal fade" id="small-modal" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+                                                                <div class="modal-dialog modal-sm modal-dialog-centered">
+                                                                    <div class="modal-content">
+                                                                        <div class="modal-header">
+                                                                            <h4 class="modal-title" id="myLargeModalLabel">Output</h4>
+                                                                            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+                                                                        </div>
+                                                                        <div class="modal-body">
+                                                                            <p> Hello Malaysia</p>
+                                                                        </div>
+                                                                        <div class="modal-footer">
+                                                                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                                                        </div>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+    
+                                                    <p align="justify">
+                                                        Anda boleh menekan butang di atas untuk melihat output yang akan dihasilkan sekiranya kita menjalankan kod tersebut di sebarang IDE.    
+                                                    </p>
+                                                </div>
+                                            </div>
+                                        </div>
+    
+    
+    
+    
+                                    </div>
+                                </div>
+                            </div>
+                           
+                        </div>
+                    </div>
+                </div>
+            </div>
         <div class="footer-wrap pd-20 mb-20 card-box">
             Crunchy Code Web Application System developed by Elysium
         </div>
--- resources/views/pages/materials/module-1-2.blade.php
+++ resources/views/pages/materials/module-1-2.blade.php
@@ -6,173 +6,55 @@
 
 
 <div class="main-container">
-		<div class="pd-ltr-20 xs-pd-20-10">
-			<div class="min-height-200px">
-				<div class="page-header">
-					<div class="row">
-						<div class="col-md-12 col-sm-12">
-							<div class="title">
-								<h4>Modul 1.2 Algoritma</h4>
-							</div>
-							<nav aria-label="breadcrumb" role="navigation">
-								<ol class="breadcrumb">
-									<li class="breadcrumb-item"><a href="index.html">Home</a></li>
-									<li class="breadcrumb-item active" aria-current="page">Algoritma</li>
-								</ol>
-							</nav>
-						</div>
-					</div>
-				</div>
-				<div class="blog-wrap">
-					<div class="container pd-0">
-						<div class="row">
+    <div class="pd-ltr-20 height-100-p xs-pd-20-10">
+            <div class="min-height-200px">
+            <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Module 1.2</title>
+    <!-- Include Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+</head>
+<body>
 
-							<!-- First Row -->
-							<div class="col-md-8 col-sm-12 mx-auto">
-								<div class="pd-20 card-box height-50-p mb-30">
-									<h4 class="mb-20 h4">Standard Kandungan Modul 1.2</h4>
-									<div class="list-group">
-										<a href="#" class="list-group-item list-group-item-action active">1.2.1 Penggunaan Algoritma untuk Menyelesaikan Masalah</a>
-										<a href="#" class="list-group-item list-group-item-action">1.2.2 Struktur Kawalan dalam Pengaturcaraan</a>
-										<a href="#" class="list-group-item list-group-item-action">1.2.3 Menguji dan Membaiki Ralat dalam Algoritma</a>
-										<a href="#" class="list-group-item list-group-item-action">1.2.4 Pengesanan Nilai Pemboleh Ubah pada Setiap Tahap dalam Algoritma</a>
-										<a href="#" class="list-group-item list-group-item-action">1.2.5 Mengenal Pasti Output yang Betul Berdasarkan Input yang Diberi</a>
-										<a href="#" class="list-group-item list-group-item-action">1.2.6 Terjemahan Algoritma ke dalam Bahasa Pengaturcaraan</a>
-									</div>
-								</div>
-							</div>
+<div class="container mt-5">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-body">
+                    <h2 class="card-title">Module 1.2: Your Module Title</h2>
+                    <p class="card-text">
+                        Welcome to Module 1.2! In this module, we will cover the following topics:
+                    </p>
+                    <ul>
+                        <li>Topic 1</li>
+                        <li>Topic 2</li>
+                        <li>Topic 3</li>
+                    </ul>
+                    <p class="card-text">
+                        This module aims to provide you with a solid understanding of these key concepts.
+                    </p>
+                    <p class="card-text">
+                        Feel free to navigate through the content and complete the associated exercises.
+                    </p>
+                    <a href="#" class="btn btn-primary">Get Started</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
-							<!-- Second Row -->
-							<div class="col-md-12 col-sm-12 mx-auto">
-								<div class="blog-detail card-box overflow-hidden mb-30">
-									<div class="blog-img">
-										<img src="vendors/images/img2.jpg" alt="">
-									</div>
-									<div class="blog-caption">
-										<h4 class="mb-10">Pengenalan  Module 1.2</h4>
-										<h5 class="mb-10">Definisi Algoritma</h5>
-										<a>Satu set arahan untuk menyelesaikan masalah. arahan -arahan terpinci yang dapat diikuti oleh pembaca sehingga tugasan terlaksana dengan jayanya adalah suatu bentuk algoritma dalam kehidupan harian. Contohnya, seorang ibu boleh dirujuk arahan arahan untuk persedian memasak dalam buku resepi.
-											Algoritma dapat dihaluskan dengan menambahkan butiran.
-										</a>
-										<a>Contoh 1 : Menjerang air
-											<ol>
-												<li>Mula</li>
-												<li>Isi air ke dalam cerek</li>
-												<li>Letakkan cerek di atas dapur gas</li>
-												<li>Hidupkan api </li>
-												<li>Tunggu sehingga air mendidih</li>
-												<li>Matikan api </li>
-											</ol>
-										</a>
-										<a>Contoh 2 : Membeli air minuman tin darioada mesin layan diri
-											<ol>
-												<li>Mula</li>
-												<li>Masukkan wang yangs ecukupnya</li></li> 
-												<li>Tekan butang air minuman tin yang dipilih</li>
-												<li>Ambil air minuman tin</li>
-												<li>Tamat</li>
-											</ol>
-										</a>
+<!-- Include Bootstrap JS and jQuery -->
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 
+</body>
+</html>
 
-										<a>Perhatikan contoh 2, adakah butiran algoritma cukup jelas ? Jika tidak, anda perlu memperhalus algoritma tersebut dengan idea untuk menyelesaikan masalah tersebut. Misalnya, arahan (2) dan (4) memerlukan butiran tambahan.</a>
-										<a>
-										Penghalusan arahan(2):
-										<ol>
-											<li>Masukkan wang syiling atau wang kertas di slot wang yang disediakan</li>
-											<li>Perhatikan baki wang yang diperlukan</li>
-											<li>Jika baki masih belum mencapai sifar, masukkan baki wang yang dikehendaki. </li>
-										</ol>
-										Penghalusan arahan (4):
-										<ol>
-											<li>Tunggu air minuman tin keluar di ruangan yang disediakan</li>
-											<li>Ambil air minuman tin tersebut </li>
-											<li>Tunggu baki wang (jika ada) di ruangan yang disediakan</li>
-										</ol>
-										</a>
-										<h5 class="mb-10">Unordered List</h5>
-										<ul>
-											<li>Duis aute irure dolor in reprehenderit in voluptate.</li>
-											<li>Sunt in culpa qui officia deserunt mollit anim id est laborum.</li>
-											<li>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.</li>
-											<li>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</li>
-											<li>Duis aute irure dolor in reprehenderit in voluptate.</li>
-											<li>Sunt in culpa qui officia deserunt mollit anim id est laborum.</li>
-											<li>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.</li>
-											<li>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</li>
-										</ul>
-										<h5 class="mb-10">Ordered List</h5>
-										<ol>
-											<li>Duis aute irure dolor in reprehenderit in voluptate.</li>
-											<li>Sunt in culpa qui officia deserunt mollit anim id est laborum.</li>
-											<li>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.</li>
-											<li>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</li>
-											<li>Duis aute irure dolor in reprehenderit in voluptate.</li>
-											<li>Sunt in culpa qui officia deserunt mollit anim id est laborum.</li>
-											<li>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.</li>
-											<li>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</li>
-										</ol>
-									</div>
-								</div>
-							</div>
-
-							<!-- <div class="col-md-4 col-sm-12">
-								<div class="card-box mb-30">
-									<h5 class="pd-20 h5 mb-0">Categories</h5>
-									<div class="list-group">
-										<a href="#" class="list-group-item d-flex align-items-center justify-content-between">HTML <span class="badge badge-primary badge-pill">7</span></a>
-										<a href="#" class="list-group-item d-flex align-items-center justify-content-between">Css <span class="badge badge-primary badge-pill">10</span></a>
-										<a href="#" class="list-group-item d-flex align-items-center justify-content-between active">Bootstrap <span class="badge badge-primary badge-pill">8</span></a>
-										<a href="#" class="list-group-item d-flex align-items-center justify-content-between">jQuery <span class="badge badge-primary badge-pill">15</span></a>
-										<a href="#" class="list-group-item d-flex align-items-center justify-content-between">Design <span class="badge badge-primary badge-pill">5</span></a>
-									</div>
-								</div>
-								<div class="card-box mb-30">
-									<h5 class="pd-20 h5 mb-0">Latest Post</h5>
-									<div class="latest-post">
-										<ul>
-											<li>
-												<h4><a href="#">Ut enim ad minim veniam, quis nostrud exercitation ullamco</a></h4>
-												<span>HTML</span>
-											</li>
-											<li>
-												<h4><a href="#">Lorem ipsum dolor sit amet, consectetur adipisicing elit</a></h4>
-												<span>Css</span>
-											</li>
-											<li>
-												<h4><a href="#">Ut enim ad minim veniam, quis nostrud exercitation ullamco</a></h4>
-												<span>jQuery</span>
-											</li>
-											<li>
-												<h4><a href="#">Lorem ipsum dolor sit amet, consectetur adipisicing elit</a></h4>
-												<span>Bootstrap</span>
-											</li>
-											<li>
-												<h4><a href="#">Lorem ipsum dolor sit amet, consectetur adipisicing elit</a></h4>
-												<span>Design</span>
-											</li>
-										</ul>
-									</div>
-								</div>
-								<div class="card-box overflow-hidden">
-									<h5 class="pd-20 h5 mb-0">Archives</h5>
-									<div class="list-group">
-										<a href="#" class="list-group-item d-flex align-items-center justify-content-between">January 2020</a>
-										<a href="#" class="list-group-item d-flex align-items-center justify-content-between">February 2020</a>
-										<a href="#" class="list-group-item d-flex align-items-center justify-content-between">March 2020</a>
-										<a href="#" class="list-group-item d-flex align-items-center justify-content-between">April 2020</a>
-										<a href="#" class="list-group-item d-flex align-items-center justify-content-between">May 2020</a>
-									</div>
-								</div>
-							</div> -->
-						</div>
-					</div>
-				</div>
-			</div>
-			<div class="footer-wrap pd-20 mb-20 card-box">
-				DeskApp - Bootstrap 4 Admin Template By <a href="https://github.com/dropways" target="_blank">Ankit Hingarajiya</a>
-			</div>
-		</div>
-	</div>
+    </div>
+</div>
 
 @endsection
