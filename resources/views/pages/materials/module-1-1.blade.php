92c0850cacf89c2a05abdc4c91553f8af72d776e
MUHAMMAD NUR SOLIHIN BIN MALIK RADZUAN <muhammadnursolihin@graduate.utm.my>
2023-11-23 00:24:22 +0800

    Penukaran
    Siap

--- resources/views/pages/materials/module-1-1.blade.php
+++ resources/views/pages/materials/module-1-1.blade.php
@@ -5,6 +5,10 @@
 @section('content')
 <style>
 
+/* Dropdown CSS */
+
+
+/* Dropdown CSS End */
 
 .card {
     display: inline-block;
@@ -262,7 +266,7 @@
         <div class="pd-ltr-20 xs-pd-20-10">
             <div class="min-height-200px">
 
-                <div class="blog-wrap">
+                <div class="blog-wrap" style="padding-bottom:0px !important">
                     <div class="container pd-0">
                         <div class="row">
                             <div class="col-md-12 col-sm-12">
@@ -376,6 +380,186 @@
                     </div>
                 </div>
             </div>
+        </div>
+
+        <!-- Module 1.1.3 -->
+        <div class="pd-ltr-20 xs-pd-20-10">
+            <div class="min-height-200px">
+
+                <div class="blog-wrap">
+                    <div class="container pd-0">
+                        <div class="row">
+                            <div class="col-md-12 col-sm-12">
+                                <div class="blog-detail card-box overflow-hidden mb-30">
+                                    
+                                    <div class="blog-caption" >
+
+                                        <!-- Pengenalan -->
+                                        <h4 class="mb-10"> Module 1.1.3 Proses Penyelesaian Masalah </h4>
+                                        <p class="text-formatting">
+                                            Proses penyelesaian masalah melibatkan langkah-langkah yang sesuai dan bergantung kepada jenis masalah yang ingin diselesaikan. Setiap masalah yang ingin diselesaikan perlu melalui beberapa proses, iaitu mengikut struktur model generik sebagai panduan. Pengaturcaraan dapat membantu murid berfikir secara kreatif dan inovatif serta membantu mereka untuk menyelesaikan masalah dan melaksanakan semua proses dengan betul. 
+                                            Model generik penyelesaian masalah dalam Rajah 1.8 terdiri daripada lapan proses. Setiap proses akan dijelaskan dengan persoalan yang harus difikirkan bersama-sama. 
+                                        </p>
+
+                                        <div class="card image-size" align="center">
+                                            <img class="card-img-top" src="../../../vendors/images/Module/penyelesaian-masalah.png" alt="Card image cap">
+                                        </div>
+
+                                        <p class="text-center" style="color:#ff0000;"> Rajah: Proses Penyelesaian Masalah </p>
+                                        
+                                        <!-- Dropdown Menu Penjelasan Proses Penyelesaian Masalah -->
+                                        <div class="col-lg-12 col-md-12 col-sm-12 mb-30">
+                                            <div class="pd-20 card-box">
+                                                <h5 class="h4 text-blue mb-20">Penjelasan Proses Penyelesaian Masalah </h5>
+                                                <div class="tab">
+                                                    <div class="row clearfix">
+                                                        <div class="col-md-4 col-sm-12">
+                                                            <ul class="nav flex-column vtabs nav-tabs customtab" role="tablist">
+                                                                <!-- Proses 1-->
+                                                                <li class="nav-item">
+                                                                    <a class="nav-link active" data-toggle="tab" href="#home4" role="tab" aria-selected="true">Kumpulkan dan analisis data</a>
+                                                                </li>
+
+                                                                <!-- Proses 2-->
+                                                                <li class="nav-item">
+                                                                    <a class="nav-link" data-toggle="tab" href="#profile4" role="tab" aria-selected="false">Tentukan masalah</a>
+                                                                </li>
+
+                                                                <!-- Proses 3-->
+                                                                <li class="nav-item">
+                                                                    <a class="nav-link" data-toggle="tab" href="#contact4" role="tab" aria-selected="false">Jana idea</a>
+                                                                </li>
+
+                                                                <!-- Proses 4-->
+                                                                <li class="nav-item">
+                                                                    <a class="nav-link" data-toggle="tab" href="#contact4" role="tab" aria-selected="false">Jana penyelesaian</a>
+                                                                </li>
+
+                                                                <!-- Proses 5-->
+                                                                <li class="nav-item">
+                                                                    <a class="nav-link" data-toggle="tab" href="#contact4" role="tab" aria-selected="false">Buat penilaian</a>
+                                                                </li>
+
+                                                                <!-- Proses 6-->
+                                                                <li class="nav-item">
+                                                                    <a class="nav-link" data-toggle="tab" href="#contact4" role="tab" aria-selected="false">Laksanakan penyelesaian</a>
+                                                                </li>
+
+                                                                <!-- Proses 7-->
+                                                                <li class="nav-item">
+                                                                    <a class="nav-link" data-toggle="tab" href="#contact4" role="tab" aria-selected="false">Tentukan tindakan</a>
+                                                                </li>
+
+                                                                 <!-- Proses 8-->
+                                                                 <li class="nav-item">
+                                                                    <a class="nav-link" data-toggle="tab" href="#contact4" role="tab" aria-selected="false">Buat penambahbaikan</a>
+                                                                </li>
+                                                            </ul>
+                                                        </div>
+                                                        <div class="col-md-8 col-sm-12">
+                                                            <div class="tab-content">
+                                                                <div class="tab-pane fade show active" id="home4" role="tabpanel">
+                                                                    <div class="pd-20">
+                                                                        Murid perlu mengumpul data tentang punca dan skop masalah serta mengenal pasti hal yang berikut dalam sesuatu situasi. 
+                                                                        <ul>
+                                                                            <li>Sejauh manakah masalah tersebut memberikan kesan?</li>
+                                                                            <li>Bilakah masalah itu perlu diselesaikan?</li>
+                                                                        </ul>
+                                                                    </div>
+                                                                </div>
+                                                                <div class="tab-pane fade" id="profile4" role="tabpanel">
+                                                                    <div class="pd-20">
+                                                                        Murid perlu mengenal pasti masalah yang paling utama yang perlu diselesaikan. Kemudian, masalah yang kedua juga perlu dikenal pasti sebelum mengenal pasti tahap pemahaman sedia ada murid. 
+                                                                    </div>
+
+                                                                    <ul>
+                                                                        <li>Adakah masalah ini merupakan masalah yang utama?</li>
+                                                                        <li>Apakah masalah lain yang dapat dikesan?</li>
+                                                                    </ul>
+
+                                                                </div>
+                                                                <div class="tab-pane fade" id="contact4" role="tabpanel">
+                                                                    <div class="pd-20">
+                                                                        Murid perlu menyenaraikan beberapa idea yang dapat digunakan untuk menyelesaikan setiap masalah yang telah dikenal pasti. 
+                                                                    </div>
+
+                                                                    <ul>
+                                                                        <li>Sejauh manakah idea yang disenaraikan dapat menyelesaikan masalah yang dihadapi?</li>
+                                                        
+                                                                    </ul>
+                                                                </div>
+
+                                                                <div class="tab-pane fade" id="contact4" role="tabpanel">
+                                                                    <div class="pd-20">
+                                                                        Murid hendaklah menyenaraikan idea atau langkah 
+                                                                        semasa merancang penyelesaian. Guru perlu mengenal pasti murid telah menjana suatu rancangan penyelesaian yang sesuai. 
+                                                                    </div>
+
+                                                                    <ul>
+                                                                        <li>Adakah idea penyelesaian ini telah dibincangkan dengan jelas?</li>
+                                                        
+                                                                    </ul>
+                                                                </div>
+
+                                                                <div class="tab-pane fade" id="contact4" role="tabpanel">
+                                                                    <div class="pd-20">
+                                                                        Murid hendaklah membuat pilihan yang terbaik daripada senarai idea yang telah dibuat. 
+                                                                    </div>
+
+                                                                    <ul>
+                                                                        <li>Adakah pilihan yang lain harus diberikan perhatian?</li>
+                                                                        <li>Dapatkah idea ini menyelesaikan masalah yang dihadapi?</li>
+                                                            
+                                                                    </ul>
+                                                                </div>
+
+                                                                <div class="tab-pane fade" id="contact4" role="tabpanel">
+                                                                    <div class="pd-20">
+                                                                        Murid perlu menggunakan pelbagai alat dan teknik yang telah dipilih untuk melaksanakan penyelesaian. Fokus hendaklah ditumpukan kepada tindakan yang boleh menyumbang kepada penyelesaian masalah. 
+                                                                    </div>
+
+                                                                    <ul>
+                                                                        <li>Bolehkah tindakan ini berjaya dilaksanakan?</li>
+                                                                    </ul>
+                                                                </div>
+
+                                                                <div class="tab-pane fade" id="contact4" role="tabpanel">
+                                                                    <div class="pd-20">
+                                                                        Penilaian akan dilaksanakan terhadap langkah-langkah penyelesaian. Jika tidak menepati tahap yang memuaskan atau tidak menyelesaikan masalah, murid perlu mencari tindakan yang lain. 
+                                                                    </div>
+
+                                                                    <ul>
+                                                                        <li>Adakah hasil yang hendak dicapai memuaskan?</li>
+                                                                    </ul>
+                                                                </div>
+
+                                                                <div class="tab-pane fade" id="contact4" role="tabpanel">
+                                                                    <div class="pd-20">
+                                                                        Setiap penyelesaian yang telah dilaksanakan perlu ditambah baik jika masih terdapat kekurangan dan mengikut keperluan. 
+                                                                    </div>
+
+                                                                    <ul>
+                                                                        <li>Apakah penambahbaikan yang boleh anda cadangkan?</li>
+                                                                    </ul>
+                                                                </div>
+
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <p class="text-formatting"> Bab ini telah merumuskan dengan terperinci keberkesanan penyelesaian masalah strategik (1.1.1), ciri-ciri penyelesaian masalah yang berkesan (1.1.2), serta penggunaan proses penyelesaian masalah yang melibatkan langkah-langkah seperti pengumpulan dan analisis data, penentuan masalah, penjanaan idea, penghapusan halangan, perumusan penyelesaian, penentuan tindakan, pelaksanaan penyelesaian, penilaian, dan penambahbaikan (1.1.3). Semua aspek ini telah dijelaskan secara komprehensif dalam bab ini, memberikan pemahaman yang mendalam terhadap konsep-konsep dan kaedah yang berkaitan dengan penyelesaian masalah dalam konteks strategik.</p>
+                                </div>
+                            </div>                         
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
         <div class="footer-wrap pd-20 mb-20 card-box">
             Crunchy Code Web Application System developed by Elysium
         </div>
