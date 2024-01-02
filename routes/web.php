1ad509f9554151e480d703119d64d1db4913feda
safurabalqis <safurabalqiss@gmail.com>
2024-01-02 22:28:08 +0800

    Adjut practice

--- routes/web.php
+++ routes/web.php
@@ -30,20 +30,16 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-<<<<<<< Updated upstream
 //Route for edit profile. Panggil ProfileController. Method yang berbeza.
 Route::get('/profile', [App\Http\Controllers\ProfileController::class, 'getProfile'])->name('profile');
 Route::post('/profile', [App\Http\Controllers\ProfileController::class, 'postProfile'])->name('profile.post');
 
 // Route for handling the photo update
 Route::post('/update-photo', [App\Http\Controllers\ProfileController::class, 'updatePhoto'])->name('update-photo');
-=======
->>>>>>> Stashed changes
 
 // Authentication Route
 Auth::routes();
 
-<<<<<<< Updated upstream
 // Route for student model (resource)
 Route::resource('user', 'App\Http\Controllers\Admin\StudentController');
 
@@ -77,17 +73,6 @@ Route::put('/quizzes/{quiz}/updateShow/{subquizId}', [QuizController::class, 'up
 
 // Route for Module function - Resource Controller
 Route::resource('modules', ModuleController::class);
-=======
-// Group the route. Apply prefix app kepada semua url
-Route::group([
-
-    'middleware' => ['auth'], // User kena login dulu baru boleh buka page
-], function () {
-
-    //Route for edit profile. Panggil ProfileController. Method yang berbeza.
-    Route::get('/profile', [App\Http\Controllers\ProfileController::class, 'getProfile'])->name('profile');
-    Route::post('/profile', [App\Http\Controllers\ProfileController::class, 'postProfile'])->name('profile.post');
->>>>>>> Stashed changes
 
 // Route for forum function - Resource Controller
 Route::resource('forum', ForumController::class);
@@ -95,16 +80,8 @@ Route::resource('forum', ForumController::class);
 // Route for forum post function - Resource Controller
 Route::resource('post', ForumPostController::class);
 
-<<<<<<< Updated upstream
 // Replace create forum route
 Route::get('/post/create/{forum_id}/{page_number}', [ForumPostController::class, 'create'])->name('post.create');
-=======
-    // Route for get the list of students
-    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-    // Route for logout into the system
-    Route::get('/logout', [LoginController::class, 'logout']);
->>>>>>> Stashed changes
 
 // Route for forum comment function - Resource Controller
 Route::resource('comment', CommentController::class);
@@ -139,53 +116,7 @@ Route::post('/contact', [ContactController::class, 'store'])->name('contact');
 //Route for message
 Route::get('/message', [MessageController::class, 'index'])->name('message');
 
-<<<<<<< Updated upstream
 //Route for delete message
 Route::delete('/admin/messages/{id}', [MessageController::class, 'destroy'])->name('destroy');
-=======
-    // Replace create forum route
-    Route::get('/post/create/{forum_id}/{page_number}', [ForumPostController::class, 'create'])->name('post.create');
-
-    // Route for forum comment function - Resource Controller
-    Route::resource('comment', CommentController::class);
-
-    // Replace create discussion route
-    Route::get('/comment/create/{post_id}/{page_number}', [CommentController::class, 'create'])->name('comment.create');
-
-    // Route for module main page
-    Route::get('/module', [App\Http\Controllers\HomeController::class, 'ViewModule'])->name('module');
-
-    // Route for chosen module page
-    Route::get('/module/{module}', [App\Http\Controllers\HomeController::class, 'showModulePage'])->name('module.page');
-
-    // Route for quiz main page
-    Route::get('/quiz', [App\Http\Controllers\HomeController::class, 'ViewQuiz'])->name('quiz');
-
-    // Route for chosen quiz page
-    Route::get('/quiz/{quizId}', [App\Http\Controllers\HomeController::class, 'showQuizPage'])->name('quiz.page');
-
-    // Route for forum main page
-    Route::get('/discussion', [App\Http\Controllers\HomeController::class, 'viewForumPage'])->name('discussion');
-
-    // Route for specific page number
-    Route::get('/forum/page/{pageNumber}/{forumTitle?}', [App\Http\Controllers\HomeController::class, 'showForumPage'])->name('forum.page');
-
-    // Route for FAQ page
-    Route::get('/FAQ', [App\Http\Controllers\HomeController::class, 'viewFAQ'])->name('FAQ');
-
-    // Route for contact us form
-    Route::post('/contact', [ContactController::class, 'store'])->name('contact');
-
-    //Route for message
-    Route::get('/message', [MessageController::class, 'index'])->name('message');
-
-    //Route for delete message
-    Route::delete('/admin/messages/{id}', [MessageController::class, 'destroy'])->name('destroy');
-
-    Route::patch('/submaterials/{submaterial}', [SubmaterialController::class, 'classupdateStatus']);
-
-
-});
->>>>>>> Stashed changes
 
 // Route::patch('/submaterials/{submaterial}', [SubmaterialController::class, 'classupdateStatus']);
