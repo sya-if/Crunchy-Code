7c0d610c71b12cb2b35ab864bf94c1b20d06f4e9
sya-if <syaiffaiz@gmail.com>
2024-01-16 16:16:30 +0800

    haha

--- database/migrations/2023_12_24_143701_create_contacts_table.php
+++ database/migrations/2023_12_24_143701_create_contacts_table.php
@@ -17,7 +17,6 @@ return new class extends Migration
             $table->id();
             $table->string('subject')->nullable();
             $table->text('message')->nullable();
-            $table->text('reply');
             // Foreign Key. Relationship between table User and table contact. Name depan kena sama dengan model
             $table->bigInteger('user_id')->unsigned();
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade'); // Declare that the user_id is inside table user and it is the id of that table
